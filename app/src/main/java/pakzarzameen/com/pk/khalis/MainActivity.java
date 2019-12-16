52617e2e05efdaac50b4c59e53515d8def304fe3
manshaw <35028949+manshaw@users.noreply.github.com>
2019-12-16 12:29:17 +0500

    init

---
+++ app/src/main/java/pakzarzameen/com/pk/khalis/MainActivity.java
@@ -0,0 +1,14 @@
+package pakzarzameen.com.pk.khalis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
